fb581e46b7f56319f8a6242fdcff25c89fa1ca76
Midka <me@midka.dev>
2022-09-28 21:52:43 +0300

    cool colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,13 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff5c00',
+        secondary: '#d764e1',
+        neutral: '#00775d',
+      },
+    },
   },
   plugins: [],
 };
