f2cde232f7c60a30963d12d25342e5ad56245fdc
Midka <me@midka.dev>
2022-09-30 17:14:46 +0300

    add: astro image compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://new.midka.dev',
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), robotsTxt(), compress()],
   vite: {
     ssr: {
-      external: ['svgo'],
-    },
+      external: ['svgo']
+    }
   },
   experimental: {
-    integrations: true,
-  },
+    integrations: true
+  }
 });
