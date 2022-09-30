55ce277ab82632fd164360c048761a416d9ee729
Midka <me@midka.dev>
2022-09-30 16:31:04 +0300

    add automatic robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
-import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import robotsTxt from 'astro-robots-txt';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: 'https://new.midka.dev',
+  integrations: [tailwind(), robotsTxt()],
   vite: {
     ssr: {
       external: ['svgo'],
     },
+  },
+  experimental: {
+    integrations: true,
   },
 });
