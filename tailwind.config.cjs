1ed487fb83e11f71f3c07fe8aad1b0195ff56b21
Midka <me@midka.dev>
2022-12-16 23:22:15 +0200

    add: some basic layout that im happy with

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,15 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
     extend: {
       colors: {
         primary: '#5fbac8',
@@ -15,9 +24,6 @@ module.exports = {
         // light: '#02c49a',
         accent: '#d473a0',
         dark: '#434343',
-      },
-      fontFamily: {
-        firacode: ['Fira Code', ...defaultTheme.fontFamily.sans],
       },
     },
   },
