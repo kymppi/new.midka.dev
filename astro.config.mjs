3c9e84d38bd8132f9e29b23530d46cc24db914fc
Midka <me@midka.dev>
2022-12-18 00:25:53 +0200

    add: prettier and astro plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import tailwind from '@astrojs/tailwind';
-import robotsTxt from 'astro-robots-txt';
-import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://new.midka.dev',
+  site: "https://new.midka.dev",
   integrations: [tailwind(), robotsTxt(), compress()],
   vite: {
     ssr: {
-      external: ['svgo']
-    }
+      external: ["svgo"],
+    },
   },
   experimental: {
-    integrations: true
-  }
+    integrations: true,
+  },
 });
