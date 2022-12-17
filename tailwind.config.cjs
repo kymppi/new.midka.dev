cd249475a4e96f8243e77f8d198b317105ef8586
Midka <me@midka.dev>
2022-12-18 00:26:33 +0200

    fix: prettier errors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,44 +1,44 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontSize: {
-      sm: '0.8rem',
-      base: '1rem',
-      xl: '1.25rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "1.953rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
     },
     extend: {
       colors: {
-        primary: '#5fbac8',
-        primarydark: '#214d53',
-        secondary: '#759e52',
-        secondarydark: '#30560f',
+        primary: "#5fbac8",
+        primarydark: "#214d53",
+        secondary: "#759e52",
+        secondarydark: "#30560f",
         // dark: '#004a3a', // #004a3a, #00775d
         // light: '#02c49a',
-        accent: '#d473a0',
-        dark: '#434343',
+        accent: "#d473a0",
+        dark: "#434343",
       },
     },
   },
   plugins: [
     function ({ addBase, theme }) {
-      function extractColorVars(colorObj, colorGroup = '') {
+      function extractColorVars(colorObj, colorGroup = "") {
         return Object.keys(colorObj).reduce((vars, colorKey) => {
           const value = colorObj[colorKey];
           const cssVariable =
-            colorKey === 'DEFAULT'
+            colorKey === "DEFAULT"
               ? `--color${colorGroup}`
               : `--color${colorGroup}-${colorKey}`;
 
           const newVars =
-            typeof value === 'string'
+            typeof value === "string"
               ? { [cssVariable]: value }
               : extractColorVars(value, `-${colorKey}`);
 
@@ -47,7 +47,7 @@ module.exports = {
       }
 
       addBase({
-        ':root': extractColorVars(theme('colors')),
+        ":root": extractColorVars(theme("colors")),
       });
     },
   ],
