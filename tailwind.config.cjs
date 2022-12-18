6cfcaebfcbe23e7932408ec9989d1dbbe0c85422
Midka <me@midka.dev>
2022-12-18 18:49:08 +0200

    hotfix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,6 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontSize: {
