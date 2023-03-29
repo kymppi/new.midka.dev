1f32c01a2f26678f4a04827bc30d840f2050fd90
Midka <me@midka.dev>
2023-03-29 21:22:55 +0300

    feat(lastfm): add basic client with typesafety

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
-
 import compress from "astro-compress";
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://new.midka.dev",
-  integrations: [robotsTxt(), compress()],
+  integrations: [robotsTxt(), compress(), svelte()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
   experimental: {
-    integrations: true,
-  },
+    integrations: true
+  }
 });
---
+++ src/components/LastFMWidget.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { LastFMTrack } from "../lib/LastFM.client";
+
+    export let data: {
+        track?: LastFMTrack;
+        user: string;
+    };
+</script>
+
+
+{#if data.track}
+	<p>{data.track.name}</p>
+{:else}
+	<p>Could not load...</p>
+{/if}
